e6da1b03894515db2987b5c054e7de9c86d00973
NLevy1 <nathanaellevy@hotmail.com>
2026-02-22 12:24:52 +0100

    Add user input function to convert celsius to fahrenheit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_project/utils/unit_converter_functions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,18 +9,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {  // This widget is the root of your application.
+  Widget build(BuildContext context) {
+    // This widget is the root of your application.
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget { // This widget is the home page of your application. It is stateful
+class MyHomePage extends StatefulWidget {
+  // This widget is the home page of your application. It is stateful
   const MyHomePage({super.key, required this.title});
   final String title;
 
@@ -29,29 +30,62 @@ class MyHomePage extends StatefulWidget { // This widget is the home page of you
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  double? _conversionResult; 
 
-  void _incrementCounter() { // This call to setState tells the Flutter framework that something has changed in this State
+  void _incrementCounter() {
+    // This call to setState tells the Flutter framework that something has changed in this State
     setState(() {
       _counter++;
     });
   }
 
   @override
-  Widget build(BuildContext context) { // This method is rerun every time setState is called,
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called,
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(backgroundColor: Colors.amber, title: Text(widget.title)),
       body: Center(
         child: Column(
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (String input) {
+                  double? parsedAmount = double.tryParse(input);
+                  if (parsedAmount != null) {
+                   double amount = parsedAmount;
+                  var result = convertCelsiusToFahrenheit(parsedAmount);
+                  setState(() {
+                    _conversionResult = result;
+                  });
+                  } else {
+                    // Added this so if the user deletes their input, the result clears
+                    setState(() {
+                      _conversionResult = null;
+                    });
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Celsius',
+                ),
+              ),
             ),
+            const SizedBox(height: 20), 
+
+            // ADD THIS: The widget that displays your result
+            Text(
+              _conversionResult != null 
+                  ? 'Result: $_conversionResult'
+                  :'Waiting for input...',
+              style: const TextStyle(
+                fontSize: 24, 
+                fontWeight: FontWeight.bold, 
+                color: Colors.deepPurple,
+              ),
+            )
           ],
         ),
       ),
--- lib/unit_converter_practice.dart
+++ lib/unit_converter_practice.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_practice_project/utils/unit_converter_functions.dart';
 import 'package:args/args.dart';
 
-void main (List<String> arguments) {
-    final parser = ArgParser()
-    ..addOption('celsius', abbr: 'c', mandatory: true);
-    final results = parser.parse(arguments);
-    String celsius_parsed = results['celsius'];
-    final celsius_double = double.parse(celsius_parsed);
+void main(List<String> arguments) {
+  final parser = ArgParser()..addOption('celsius', abbr: 'c', mandatory: true);
+  final results = parser.parse(arguments);
+  String celsius_parsed = results['celsius'];
+  final celsius_double = double.parse(celsius_parsed);
 
-    print(convertCelsiusToFahrenheit(celsius_double));
+  print(convertCelsiusToFahrenheit(celsius_double));
 }
